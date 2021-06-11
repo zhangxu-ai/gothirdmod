266d64c09e0ae5bf9a6e76f3e88b1fc0f0c88375
julinzhang <julinzhang@tencent.com>
2021-06-11 20:16:13 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zhangxu-ai/thirdmod
+
+go 1.15
+
+require github.com/zhangxu-ai/gomutimod v1.0.0
---
+++ third.go
@@ -0,0 +1,11 @@
+package third
+
+import (
+	"fmt"
+	a "github.com/zhangxu-ai/gomutimod/mods/a/a1"
+)
+
+func Go(){
+	fmt.Println("in third mod")
+	a.A()
+}
